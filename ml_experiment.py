db0a2299628db38b49a07ffde781dbbd8cfa37a6
shaileshpilare <shailesh.pilare@broadcom.com>
2020-07-28 22:17:12 -0700

    Linear Regression for NYC Taxi Data Analysis
    Signed-off-by: shaileshpilare <shailesh.pilare@gmail.com>

--- ml_experiment.py
+++ ml_experiment.py
@@ -1,17 +1,8 @@
-import sys
 from pyspark.sql import SparkSession
 from pyspark.ml.feature import StringIndexer,VectorIndexer, VectorAssembler
-from pyspark.sql.types import IntegerType
-import pyspark.sql
 from pyspark.ml import Pipeline
-
 from pyspark.ml.regression import LinearRegression
-from pyspark.ml.regression import GeneralizedLinearRegression
-from pyspark.ml.regression import DecisionTreeRegressor
-from pyspark.ml.regression import RandomForestRegressor
-from pyspark.ml.regression import GBTRegressor
 from pyspark.ml.evaluation import RegressionEvaluator
-
 
 if __name__ == "__main__":
 
@@ -28,32 +19,24 @@ if __name__ == "__main__":
     paymentIndexer = StringIndexer(inputCol="payment_type", outputCol="payment_indexed").setHandleInvalid("skip")
     vendorIndexer = StringIndexer(inputCol="vendor_name", outputCol="vendor_indexed").setHandleInvalid("skip")
 
+    
     assembler = VectorAssembler(inputCols=["passenger_count", "trip_distance","hour","day_of_week_new","start_cluster", "payment_indexed", "vendor_indexed"], outputCol="features")
 
     (trainingData, testData) = df_102.randomSplit([0.7, 0.3])
 
-    modelType = str(sys.argv[1])
-    maxIter = float(sys.argv[2])
-    regParam = float(sys.argv[3])
-    elasticNetParam = float(sys.argv[4])
-
-
-
-
-    lr = LinearRegression(maxIter=maxIter, regParam=regParam, elasticNetParam=elasticNetParam)
-
+    lr = LinearRegression(maxIter=10, regParam=0.3, elasticNetParam=0.8)
     pipeline = Pipeline(stages=[paymentIndexer, vendorIndexer, assembler, lr])
-
 
     grModel = pipeline.fit(trainingData)
     df_final=grModel.transform(testData)
     evaluator=RegressionEvaluator(labelCol="label", predictionCol="prediction", metricName="r2")
-    #r2=evaluator.evaluate(df_final)
+
     rmse = evaluator.evaluate(df_final, {evaluator.metricName: "rmse"})
     r2 = evaluator.evaluate(df_final, {evaluator.metricName: "r2"})
 
     print('RMSE Linear: ' + str(rmse))
     print('R^2: Linear' + str(r2))
+
 
 
 
